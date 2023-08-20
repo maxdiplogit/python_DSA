f2b91367b5e5991933314c5d7eff5f1a6239178d
Harshmeet Singh <maxdiplo@Harshmeets-MacBook-Air.local>
2023-08-20 15:44:34 +0530

    add backspaceCompare.py

--- TwoPointers/backspaceCompare.py
+++ TwoPointers/backspaceCompare.py
@@ -1,1 +1,21 @@
-print('Hello World')
+string1 = input()
+string2 = input()
+
+res1 = ""
+res2 = ""
+
+for i in range(len(string1)):
+    if (string1[i] != '#'):
+        res1 += string1[i]
+    else:
+        res1 = res1[:-1]
+
+for i in range(len(string2)):
+    if (string2[i] != '#'):
+        res2 += string2[i]
+    else:
+        res2 = res2[:-1]
+
+
+print(res1)
+print(res2)
